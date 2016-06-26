35437220798c611eb99b5afe42d5c7adbf6d9215
Jordan Camp <jordan.camp748@gmail.com>
2016-06-26 13:11:49 -0500

    beat numbers displayed

--- js/metronome.js
+++ js/metronome.js
@@ -4,11 +4,91 @@ var audioContext = null;  // audio context
 var thickness = 25;   // the thickness of the egede of the metronome box
 var lookahead = 25.0;       // How frequently to call scheduling function 
 var isPlaying = false;
+var scheduleAheadTime = 0.1;    // How far ahead to schedule audio (sec)
+var tempo = 120.0;          // tempo (in beats per minute)
+var nextNoteTime = 0.0;     // when the next note is due.
+var currentBeat = 0;
+var beatUnit = 4;           //the bottom number of a time signature
+var beatsPerMeasure = 4;    //the top number of a time signature
+var notesInQueue = [];      // the notes that have been put into the web audio,
+var noteLength = 0.05;      // length of "beep" (in seconds)
 
 // colorscheme for metronome screen
 var backgroundColor = "rgba(196, 226, 196, 1)";
 var segmentOn = "rgba(9, 9, 9, 1)";
 var segmentOff = "rgba(175, 203, 175, 1)";
+
+
+function play() {
+    isPlaying = !isPlaying;
+
+    if (isPlaying) { // start playing
+        nextNoteTime = audioContext.currentTime;
+        timerWorker.postMessage("start");
+        return "stop";
+    } else {
+        timerWorker.postMessage("stop");
+        return "play";
+    }
+}
+
+function nextNote() {
+    // Advance current note and time by a 16th note...
+    var secondsPerBeat = 60.0 / tempo;    // Notice this picks up the CURRENT 
+                                          // tempo value to calculate beat length.
+    nextNoteTime += secondsPerBeat;       // Add beat length to last beat time
+
+    currentBeat++;    // Advance the beat number, wrap to zero
+    if (currentBeat == beatsPerMeasure) {
+        currentBeat = 0;
+    }
+}
+
+function scheduleNote( beatNumber, time ) {
+    // push the note on the queue, even if we're not playing.
+    notesInQueue.push( { note: beatNumber, time: time } );
+
+    // create an oscillator
+    var osc = audioContext.createOscillator();
+    osc.connect( audioContext.destination );
+    //console.log(beatNumber);
+    if (beatNumber === 0)    // beat 0 == high pitch
+        osc.frequency.value = 880.0;
+    else                        // other 16th notes = low pitch
+        osc.frequency.value = 440.0;
+
+    osc.start( time );
+    osc.stop( time + noteLength );
+}
+
+function scheduler() {
+    // while there are notes that will need to play before the next interval, 
+    // schedule them and advance the pointer.
+    while (nextNoteTime < audioContext.currentTime + scheduleAheadTime ) {
+        scheduleNote( currentBeat, nextNoteTime );
+        nextNote();
+    }
+}
+
+function draw() {
+  // do drawing stuff
+  drawBox(); 
+  drawDisplay();
+  window.requestAnimationFrame(draw);
+}
+
+function drawBox() {
+  roundedRect(ctx, 0, 0, 600, 400, 12, "black");
+  roundedRect(ctx, thickness, thickness, 550, 350, 12, backgroundColor);
+}
+
+function drawDisplay() {
+  var displayBeat = currentBeat;
+  if(currentBeat === 0)
+    displayBeat = 4;
+  display.setValue(displayBeat.toString());
+}
+
 
 // create <canvas> element and add it to document
 function init() {
@@ -42,45 +122,14 @@ function init() {
 
   timerWorker.onmessage = function(e) {
       if (e.data == "tick") {
-          console.log("tick!");
-          //scheduler();
+          //console.log("tick!");
+          scheduler();
       }
       else
           console.log("message: " + e.data);
   };
   timerWorker.postMessage({"interval":lookahead});
 
-}
-
-function play() {
-    isPlaying = !isPlaying;
-
-    if (isPlaying) { // start playing
-        nextNoteTime = audioContext.currentTime;
-        timerWorker.postMessage("start");
-        return "stop";
-    } else {
-        timerWorker.postMessage("stop");
-        return "play";
-    }
-}
-
-function draw() {
-  // do drawing stuff
-  drawBox(); 
-  drawDisplay();
-  // update anim for next frame
-  update();
-  window.requestAnimationFrame(draw);
-}
-
-function drawBox() {
-  roundedRect(ctx, 0, 0, 600, 400, 12, "black");
-  roundedRect(ctx, thickness, thickness, 550, 350, 12, backgroundColor);
-}
-
-function drawDisplay() {
-  display.setValue('1');
 }
 
 // utility function for drawing rectangles with rounded corners
@@ -100,12 +149,6 @@ function roundedRect(ctx,x,y,width,height,radius, color){
   ctx.stroke();
   ctx.fill();
   ctx.restore();
-}
-
-function update() {
-  // update the animation for the next frame
-
-
 }
 
 window.onload = function() {
