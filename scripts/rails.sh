1c942a3ba9f26b9e420597c0e66480e86b8133f2
Jordan Camp <jordan.camp748@gmail.com>
2016-08-29 20:53:37 -0500

    fixed path on rails.sh

--- scripts/rails.sh
+++ scripts/rails.sh
@@ -5,5 +5,5 @@ find rails.sh . > /dev/null
 cd ..
 FILE=$(pwd)/js/metronome.js
 sed -i.bak -e's/js\/metronomeworker.js/\/javascripts\/advanced-metronome\/js\/metronomeworker.js/' $FILE 
-rm -rf tests/
+rm -rf test/
 rm -rf scripts/
