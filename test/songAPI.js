a74a7f082ab3cda8fcc3448f5535324fe6dbf400
Jordan Camp <jordan.camp748@gmail.com>
2017-08-06 15:07:21 -0500

    created skeleton of songAPI test file

---
+++ test/songAPI.js
@@ -0,0 +1,41 @@
+//test api utility function for song.js
+QUnit.test( "verify control functionality", function( assert ) {
+
+
+//use the data hard coded into the song.js file itself
+        //"0" : {
+          //"timesig" : "4/4",
+          //"tempo" : "100",
+          //"count" : "1",
+          //"section" : "lead in"
+        //},
+        //"1" : {
+          //"timesig" : "4/4",
+          //"tempo" : "100",
+          //"count" : "2",
+          //"section" : "riff a"
+        //},
+        //"2" : {
+          //"timesig" : "3/4",
+          //"tempo" : "200",
+          //"count" : "3",
+          //"section" : "riff b"
+        //},
+        //"3" : {
+          //"timesig" : "4/4",
+          //"tempo" : "150",
+          //"count" : "2",
+          //"section" : "riff c"
+        //}
+        
+// getBeat function
+
+// getTempo function
+// getCount function
+// getTimeSig function
+// getSectionName function
+// getMeasureNumber function
+// getMeasureData function
+// refresh function
+
+});
