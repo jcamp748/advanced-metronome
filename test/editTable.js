a5d98561c91862511293883069d7ba7d9da26d73
Jordan Camp <jordan.camp748@gmail.com>
2017-08-17 13:23:09 -0500

    tests written for highlight row function

--- test/editTable.js
+++ test/editTable.js
@@ -29,9 +29,25 @@ define(["qunit", "app/song", "app/metronomeTable"], function(QUnit, song, table)
       }
     };
 
+    function verifyForm() {
+      // verify the fields in the form match the highlighted row data
+
+    }
+
     window.song = song.getInstance(testObj);
     // make sure window.song object exists
     assert.notEqual(window.song, undefined, "window.song exists");
+    assert.equal( window.song.measures.length, 7, "song starts with 7 measures");
+
+    // test row click event
+    var $firstRow = $("#metronomeTable tbody").children(":nth-child(1)");
+    var $secondRow = $("#metronomeTable tbody").children(":nth-child(2)");
+    $firstRow.click();
+    assert.ok($firstRow.hasClass("highlight"), "the first row is highlighted");
+    $secondRow.click();
+    assert.notOk($firstRow.hasClass("highlight"), "the first row is NOT highlighted");
+    assert.ok($secondRow.hasClass("highlight"), "the second row is highlighted");
+
 
 
   });
