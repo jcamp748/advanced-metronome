8a0e04c25b39e54bfddbe676fdfd05214052f75c
Jordan Camp <jordan.camp748@gmail.com>
2017-08-17 14:15:17 -0500

    figure out the riff a and b problem

--- test/editTable.js
+++ test/editTable.js
@@ -29,9 +29,39 @@ define(["qunit", "app/song", "app/metronomeTable"], function(QUnit, song, table)
       }
     };
 
+    function getInputData(index) {
+      // get the data for the row at INDEX and return it
+      
+      //:nth-child is indexed at 1 so we need to inc the index
+      index++;
+      var selector = ":nth-child(" + index.toString() + ")";
+      var $row = $("#metronomeForm").children(selector);
+      return $row.children("input").val();
+    }
+
+    function getRowData() {
+      // get data from first 4 columns of the highlighted row
+      // and return it as an array of strings
+      var data = [];
+
+      var $row = $("#metronomeTable tbody").children(".highlight");
+      $row.children().each(function() {
+        // this is the current DOM element in the loop
+        data.push($(this).text());
+      });
+      return data;
+    }
+
     function verifyForm() {
       // verify the fields in the form match the highlighted row data
-
+      var match = true;
+      var rowData = getRowData();
+      for(var i = 0, len = rowData.length; i < len; i++) {
+        if(rowData[i] != getInputData(i)) {
+          match = false;
+        }
+      }
+      return match;
     }
 
     window.song = song.getInstance(testObj);
@@ -44,9 +74,11 @@ define(["qunit", "app/song", "app/metronomeTable"], function(QUnit, song, table)
     var $secondRow = $("#metronomeTable tbody").children(":nth-child(2)");
     $firstRow.click();
     assert.ok($firstRow.hasClass("highlight"), "the first row is highlighted");
+    assert.ok(verifyForm(), "form data matches row data");
     $secondRow.click();
     assert.notOk($firstRow.hasClass("highlight"), "the first row is NOT highlighted");
     assert.ok($secondRow.hasClass("highlight"), "the second row is highlighted");
+    assert.ok(verifyForm(), "form data matches row data");
 
 
 
