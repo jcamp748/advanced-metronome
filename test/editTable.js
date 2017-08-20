2882aa52817a9e98f0469eb75042922a31c35030
Jordan Camp <jordan.camp748@gmail.com>
2017-08-20 16:13:17 -0500

    write more tests for metronomeTable.js

--- test/editTable.js
+++ test/editTable.js
@@ -64,6 +64,10 @@ define(["qunit", "app/song", "app/metronomeTable"], function(QUnit, song, table)
       return match;
     }
 
+    function getMeasure(index) {
+      return window.song.metronomeData[index];
+    }
+
     window.song = song.getInstance(testObj);
     table.initialize();
     // make sure window.song object exists
@@ -96,13 +100,49 @@ define(["qunit", "app/song", "app/metronomeTable"], function(QUnit, song, table)
     var firstSection = {
         "timesig" : "2/4",
         "tempo" : "220",
-        "count" : "3",
+        "count" : "1",
         "section" : "my first section"
       };
     table.unshift(firstSection);
+    assert.deepEqual(getMeasure(1), firstSection, "unshift first section");
     // test push
+    var pushSection = {
+        "timesig" : "7/4",
+        "tempo" : "250",
+        "count" : "1",
+        "section" : "my push section"
+      };
+    table.push(pushSection);
+
+    // utility method for getting last index from metronomeData
+    function getLastIndex() {
+      return Object.keys(window.song.metronomeData).length;
+    }
+
+    assert.deepEqual(getMeasure(getLastIndex()), pushSection, "last section should be push section");
     // test insert
+    var insertSection = {
+        "timesig" : "5/8",
+        "tempo" : "123",
+        "count" : "1",
+        "section" : "my first section"
+      };
+    var oldMeasure6 = getMeasure(6);
+    var oldMeasure7 = getMeasure(7);
+    var lastMeasure = getMeasure(getLastIndex());
+    table.insert(insertSection, 7);
+    assert.deepEqual(getMeasure(7), insertSection, "insertSection should be at index 7");
+    assert.deepEqual(getMeasure(8), oldMeasure7, "section seven should now be 8"); 
+    assert.deepEqual(getMeasure(6), oldMeasure6, "section six should be the same as it was");
+    assert.deepEqual(getMeasure(getLastIndex()), lastMeasure, "last measure should be the same");
     // test deleteRow
+    var oldMeasure2 = getMeasure(2);
+    lastMeasure = getMeasure(getLastIndex());
+    // delete 'my first section'
+    $("#metronomeTable tbody").children(":nth-child(2)").click();  
+    table.deleteRow();
+    assert.deepEqual(getMeasure(1), oldMeasure2, "measure 2 should now be 1");
+    assert.deepEqual(getMeasure(getLastIndex()), lastMeasure, "last measure should still be the same");
     // test updateRow
     
 
